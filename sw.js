e99245e936d7ff67ea2437dd6de919e8eff50410
Bhaltazar <251271036@alumnos.utn.edu.mx>
2026-05-28 17:04:16 -0600

    CAMBIO 1

--- sw.js
+++ sw.js
@@ -1,13 +1,24 @@
-const CACHE_NAME = 'monify-v1';
+const CACHE_NAME = 'monify-v2';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './app.js',
+  './styles.css'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
 
